16487d30d24f8b923a92adb9f56d87ba480c9654
abdullahwale <abdullahwale@gmail.com>
2021-08-01 16:07:01 +0500

    First Practice App
    Load my Image from Internet

---
+++ practice_first_basic_app/android/app/src/main/kotlin/com/example/practice_first_basic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practice_first_basic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ practice_first_basic_app/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Text("Abdullah"),
+    ),
+  ));
+}
+
+//void main() => runApp(MyApp());
+
+// void main() {
+//   runApp(MyApp());
+//   // print("Great Feature");
+//   // print("Muhammad Abdullah Wali");
+// }
+
+// class MyApp extends StatelessWidget {
+//   //@override
+//   Widget build(BuildContext context) {
+//     final title = 'Load My Image App';
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(title),
+//         ),
+//         body: Center(
+//           child: Image.network(
+//               'https://avatars0.githubusercontent.com/u/39946504?s=400&u=44764b768976a6b3633ee5671fbfe9dc355db83f&v=4'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+/*
+class MyApp extends StatelessWidget {
+  //@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(child: Text("Abdullah")),
+    );
+  }
+}
+ */
