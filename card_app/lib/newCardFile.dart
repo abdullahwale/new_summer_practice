53c5229574e96586522a62859140e6cb6a26dad3
abdullahwale <abdullahwale@gmail.com>
2021-09-30 09:49:30 +0500

    Card App New

---
+++ card_app/lib/newCardFile.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 70.0,
+              backgroundImage: AssetImage('image/abc.png'),
+            ),
+            Text(
+              'Sports Arena',
+              style: TextStyle(
+                fontFamily: 'Stick No Bills',
+                fontSize: 40.0,
+                color: Colors.lightGreenAccent,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Pakistan',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.zoom_out_rounded,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'aareena.sports@email.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
