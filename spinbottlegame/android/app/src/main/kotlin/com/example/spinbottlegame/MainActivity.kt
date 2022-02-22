5cf70f1abb7e5b46c7422c87dde5e4b3c4c64de4
abdullahwale <abdullahwale@gmail.com>
2022-02-22 10:09:56 +0500

    Spin Bottle Game

---
+++ spinbottlegame/android/app/src/main/kotlin/com/example/spinbottlegame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spinbottlegame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ spinbottlegame/lib/main.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  var lastPosition = 0.0;
+  var random = new Random();
+
+  AnimationController _animationController;
+  Animation _animation;
+
+  double getRandom() {
+    return random.nextDouble();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              "images/floor.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Center(
+            child: Container(
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: getRandom()).animate(
+                    new CurvedAnimation(
+                        parent: _animationController, curve: Curves.linear)),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _animationController = AnimationController(
+                          vsync: this, duration: Duration(seconds: 1));
+                      _animationController.forward();
+                    });
+                  },
+                  child: Image.asset(
+                    "images/bottle.png",
+                    width: 250,
+                    height: 250,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
